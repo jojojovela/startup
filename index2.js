c58c6062d16b53b65aec3000e5b3ac08f4bf71ba
Josimar Vela <vel21016@byui.edu>
2024-02-27 09:58:20 -0700

    updated js file

--- index2.js
+++ index2.js
@@ -1,18 +1,9 @@
-var currentRow = 1; // Variable to keep track of the current row
-var currentCell = 0; // Variable to keep track of the current cell in the row
-
 function uploadImage() {
   var fileInput = document.getElementById("fileInput");
   var imageGrid = document.getElementById("imageGrid");
 
   // Check if a file is selected
   if (fileInput.files.length > 0) {
-    // Check if a new row needs to be created
-    if (currentCell >= 3) {
-      currentRow++;
-      currentCell = 0;
-    }
-
     // Create a new image element
     var newImage = document.createElement("img");
     newImage.src = URL.createObjectURL(fileInput.files[0]);
@@ -22,18 +13,10 @@ function uploadImage() {
     var newCell = document.createElement("td");
     newCell.appendChild(newImage);
 
-    // Check if a new row needs to be created in the grid
-    if (currentCell === 0) {
-      var gridRow = document.createElement("tr");
-      gridRow.id = "row" + currentRow; // Assign an id to the row for future reference
-      imageGrid.appendChild(gridRow);
-    }
-
     // Append the new cell to the grid
-    document.getElementById("row" + currentRow).appendChild(newCell);
-
-    // Increment the current cell
-    currentCell++;
+    var gridRow = document.createElement("tr");
+    gridRow.appendChild(newCell);
+    imageGrid.appendChild(gridRow);
 
     // Clear the file input for convenience
     fileInput.value = "";
